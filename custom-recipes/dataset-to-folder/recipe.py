07af8701f34e52f5f6e3b1ab53e80f1a0fb75f3a
pierrepetrella <pierre.petrella@dataiku.com>
2022-05-12 15:49:36 +0100

    Edited file 'custom-recipes/dataset-to-folder/recipe.py'

--- custom-recipes/dataset-to-folder/recipe.py
+++ custom-recipes/dataset-to-folder/recipe.py
@@ -43,7 +43,7 @@ def write_df_in_wb(df):
     return wb
 
 def write_csv_to_managed_folder (managed_folder_handle, df, file_path):
-    with managed_folder_handle.get_writer(managed_folder_handle) as writer:
+    with managed_folder_handle.get_writer(file_path) as writer:
         writer.write(Final_df1.to_csv().encode("utf-8"))
 
 
