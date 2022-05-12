cbbacb171c90b90cfa20af7a531cbf5589c81e39
pierrepetrella <pierre.petrella@dataiku.com>
2022-05-12 15:43:07 +0100

    Edited file 'custom-recipes/dataset-to-folder/recipe.py'

--- custom-recipes/dataset-to-folder/recipe.py
+++ custom-recipes/dataset-to-folder/recipe.py
@@ -17,16 +17,11 @@ input_dataset = dataiku.Dataset(input_dataset_name)
 input_dataset_df = input_dataset.get_dataframe()
 
 # Get handle on output dataset name to feed to the "COPY" query
-print ("get_output_names_for_role :", get_output_names_for_role('output_folder'))
-
-print ("get_output_names :", get_output_names('output_folder'))
-
 output_folder_id = get_output_names('output_folder')[0].split(".")[1]
 output_folder = dataiku.Folder(output_folder_id)
 
 
 # Get handle on the PARAMS
-
 file_path = get_recipe_config().get('file_path', None)
 file_type = get_recipe_config().get('file_type', None)
 
@@ -53,9 +48,9 @@ def write_csv_to_managed_folder (df, file_path):
 
 
 if (file_type == "csv"):
-    write_csv_to_managed_folder (input_dataset_df, output_file_path)
+    write_csv_to_managed_folder (input_dataset_df, file_path)
 elif (file_type == "excel"):
     wb = write_df_in_wb(input_dataset_df)
-    write_wb_to_managed_folder(wb, output_file_path) 
+    write_wb_to_managed_folder(wb, file_path) 
 else:
     raise Exception ("Export file type : " + file_type + " is not supported")
